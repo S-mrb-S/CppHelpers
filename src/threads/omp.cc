34eca77e95eec7cfba6e5f4108d7ad01f1c3272b
MRB <mehrabshafae@gmail.com>
2024-10-20 02:08:02 +0330

    mrb: add tasks with delay to omp

--- src/threads/omp.cc
+++ src/threads/omp.cc
@@ -1,19 +1,5 @@
 // OpenMP
 
-    // #pragma omp parallel
-    // {
-    //     #pragma omp single // فقط یک رشته این بخش را اجرا می‌کند
-    //     {
-    //         std::vector<omp_task_t> tasks; // لیست کارها
-
-    //         for (int i = 0; i < 5; ++i) {
-    //             #pragma omp task // ایجاد یک کار جدید
-    //             taskFunction(i);
-    //         }
-    //         #pragma omp taskwait // منتظر ماندن برای اتمام کارها
-    //     }
-    // }
-
 // ===========================================================
 #pragma once
 #include "../core/cc.hpp"
@@ -26,8 +12,10 @@ public:
     template <typename Func>
     FireAndForget &operator>>(Func &&func)
     {
-        #pragma omp task
+#pragma omp parallel
+#pragma omp single
         {
+#pragma omp task
             func();
         }
         return *this;
@@ -36,7 +24,7 @@ public:
     // انتظار برای تمام تسک‌ها
     void wait()
     {
-        #pragma omp taskwait
+#pragma omp taskwait
     }
 };
 
@@ -45,24 +33,28 @@ void fire_test()
 {
     FireAndForget fire;
 
-    fire >> [] {
+    fire >> []
+    {
         std::cout << "Task 1 executed\n";
-    } >> [] {
+    } >> []
+    {
         std::cout << "Task 2 executed\n";
-    } >> [] {
+    } >> []
+    {
         std::cout << "Immediate Task executed\n";
     };
 
     // صبر کردن برای اتمام تسک‌ها
-    // std::cout << "Waiting for all tasks to complete...\n";
-    // fire.wait();
+    std::cout << "Waiting for all tasks to complete...\n";
+    fire.wait();
 
     std::cout << "All tasks completed.\n";
 }
 
-int main() {
+int main()
+{
     // تنظیم تعداد رشته‌های OpenMP
-    omp_set_dynamic(0); // Disable dynamic adjustment of the number of threads
+    omp_set_dynamic(0);     // Disable dynamic adjustment of the number of threads
     omp_set_num_threads(4); // Set number of threads
 
     fire_test();
--- test/mp2.cc
+++ test/mp2.cc
@@ -1,17 +1,61 @@
 #include <iostream>
+#include <vector>
 #include <omp.h>
+#include <chrono>
+#include <thread> // برای استفاده از std::this_thread::sleep_for
 
-int main() {
-    const int N = 100000;
-    long long sum = 0;
-
-    // Compute the sum of integers from 1 to N in parallel using OpenMP
-    #pragma omp parallel for reduction(+:sum)
-    for (int i = 1; i <= N; ++i) {
-        sum += i;
+class FireAndForget
+{
+public:
+    // اجرای تسک با استفاده از OpenMP
+    template <typename Func>
+    FireAndForget &operator>>(Func &&func)
+    {
+#pragma omp task
+        {
+            func(); // اجرای تابع
+        }
+        return *this;
     }
 
-    std::cout << "The sum of integers from 1 to " << N << " is " << sum << std::endl;
+    // انتظار برای تمام تسک‌ها
+    void wait()
+    {
+#pragma omp taskwait
+    }
+};
 
+// تابع برای تأخیر در اجرای تسک
+void delayed_task(int id, int delay)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(delay)); // تأخیر
+    std::cout << "Task " << id << " executed after " << delay << " ms\n";
+}
+
+// تست FireAndForget با OpenMP
+void fire_test()
+{
+    FireAndForget fire;
+
+    // اضافه کردن تسک‌ها با تأخیرهای متفاوت
+    fire >> [] { delayed_task(1, 1000); } // تسک 1 با تأخیر 1000 میلی‌ثانیه
+        >> [] { delayed_task(2, 500); }   // تسک 2 با تأخیر 500 میلی‌ثانیه
+        >> [] { delayed_task(3, 300); }   // تسک 3 با تأخیر 300 میلی‌ثانیه
+        >> [] { delayed_task(4, 200); };  // تسک 4 با تأخیر 200 میلی‌ثانیه
+
+    // صبر کردن برای اتمام تسک‌ها
+    std::cout << "Waiting for all tasks to complete...\n";
+    fire.wait();
+
+    std::cout << "All tasks completed.\n";
+}
+
+int main()
+{
+    // تنظیم تعداد رشته‌های OpenMP
+    omp_set_dynamic(0);     // غیرفعال کردن تنظیم دینامیک تعداد رشته‌ها
+    omp_set_num_threads(4); // تنظیم تعداد رشته‌ها
+
+    fire_test();
     return 0;
 }
