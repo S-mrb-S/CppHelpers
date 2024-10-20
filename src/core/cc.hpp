a089cde2afa424f6407f1be3ede6d16f2b6edfae
MRB <mehrabshafae@gmail.com>
2024-10-20 21:58:29 +0330

    mrb: cu header

--- src/core/cc.hpp
+++ src/core/cc.hpp
@@ -58,9 +58,9 @@ using std::runtime_error;
 // src
 #include "../macros/macros.cpp"
 #include "../let/let.cpp"
-#include "../threads/async.cpp"
-#include "../threads/jthread.cpp"
-#include "../threads/fire.cc"
+// #include "../threads/async.cpp"
+// #include "../threads/jthread.cpp"
+#include "../threads/omp.cc"
 #include "../var/var.cpp"
 
 
